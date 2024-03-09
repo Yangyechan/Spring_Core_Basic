16c500517113b9814d892b130b928f736c5a4c19
yangyechan <yac392045@mokpo.ac.kr>
2024-03-09 23:21:58 +0900

    "first commit"

---
+++ src/main/java/com/example/inflearn_1/Inflearn1Application.java
@@ -0,0 +1,13 @@
+package com.example.inflearn_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inflearn1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Inflearn1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inflearn_1/Inflearn1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearn_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inflearn1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
