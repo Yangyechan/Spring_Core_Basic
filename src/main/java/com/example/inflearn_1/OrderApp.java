9b5127ea1edd60e8aa4edd65d82d8c47cb8643a3
yangyechan <yac392045@mokpo.ac.kr>
2024-03-13 10:47:35 +0900

    "lecture_3 스프링 적용"

--- src/main/java/com/example/inflearn_1/OrderApp.java
+++ src/main/java/com/example/inflearn_1/OrderApp.java
@@ -7,14 +7,20 @@ import com.example.inflearn_1.member.MemberServiceImpl;
 import com.example.inflearn_1.order.Order;
 import com.example.inflearn_1.order.OrderService;
 import com.example.inflearn_1.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 //        MemberService memberService = new MemberServiceImpl(null);
 //        OrderService orderService = new OrderServiceImpl(null, null);
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
