c5fd9fb21d529466c543a0fe848c70ab79af368b
yangyechan <yac392045@mokpo.ac.kr>
2024-03-20 21:18:24 +0900

    "lecture_8 빈 스코프"

---
+++ src/main/java/com/example/inflearn_1/web/LogDemoService.java
@@ -0,0 +1,16 @@
+package com.example.inflearn_1.web;
+
+import com.example.inflearn_1.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
